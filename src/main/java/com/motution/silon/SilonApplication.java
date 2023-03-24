21b4ad72275d83cf85a6c8dcf05ffc099d208a6d
bijayplates <99069889+bijayplates@users.noreply.github.com>
2023-03-24 11:18:28 +0530

    Initial commit

---
+++ src/main/java/com/motution/silon/SilonApplication.java
@@ -0,0 +1,13 @@
+package com.motution.silon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilonApplication.class, args);
+	}
+
+}
--- src/test/java/com/motution/silon/SilonApplicationTests.java
+++ src/test/java/com/motution/silon/SilonApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.motution.silon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemocrudApplicationTests {
+class SilonApplicationTests {
 
 	@Test
 	void contextLoads() {
